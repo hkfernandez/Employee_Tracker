186d9c91228d14f4a001d83a5cf91764746c8539
Hector Fernandez <hkfernandezdev@gmail.com>
2021-02-04 22:42:36 -0600

    index.js - new records adding to dept table

--- index.js
+++ index.js
@@ -74,6 +74,15 @@ function addRole () {
             })
 }
 
+function addDept () {
+      inquirer.prompt (questions.addDept)
+            .then 
+            (({name})=>{
+                  let newDept = new Dept (name);
+                  insertNewRecord(newDept, 'dept');
+            })
+}
+
 function insertNewRecord (newObject, tableName){
       console.log(`Inserting a new record in ${tableName}...\n`);
       var query = connection.query(
@@ -90,15 +99,6 @@ function insertNewRecord (newObject, tableName){
       console.log(query.sql);
     }
 
-// function addDept () {
-//       inquirer.prompt (questions.addDept)
-//             .then 
-//             (({name})=>{
-//                   let dept = new Dept (name);
-//                   pushNewDept(dept);
-
-//             })
-// }
 
 // function pushNewDept (){}
 
@@ -137,75 +137,75 @@ function insertNewRecord (newObject, tableName){
 //  init ();
 
 //=======================CRUD FUNCTIONS=========================================
-function addDept() {
-      console.log("Inserting a new product...\n");
-      var query = connection.query(
-        "INSERT INTO products SET ?",
-        {
-          flavor: "Rocky Road",
-          price: 3.0,
-          quantity: 50
-        },
-        function(err, res) {
-          if (err) throw err;
-          console.log(res.affectedRows + " product inserted!\n");
-          // Call updateProduct AFTER the INSERT completes
-          updateProduct();
-        }
-      );
+// function addDept() {
+//       console.log("Inserting a new product...\n");
+//       var query = connection.query(
+//         "INSERT INTO products SET ?",
+//         {
+//           flavor: "Rocky Road",
+//           price: 3.0,
+//           quantity: 50
+//         },
+//         function(err, res) {
+//           if (err) throw err;
+//           console.log(res.affectedRows + " product inserted!\n");
+//           // Call updateProduct AFTER the INSERT completes
+//           updateProduct();
+//         }
+//       );
     
-      // logs the actual query being run
-      console.log(query.sql);
-    }
+//       // logs the actual query being run
+//       console.log(query.sql);
+//     }
     
-    function updateEmployee() {
-      console.log("Updating all Rocky Road quantities...\n");
-      var query = connection.query(
-        "UPDATE products SET ? WHERE ?",
-        [
-          {
-            quantity: 100
-          },
-          {
-            flavor: "Rocky Road"
-          }
-        ],
-        function(err, res) {
-          if (err) throw err;
-          console.log(res.affectedRows + " products updated!\n");
-          // Call deleteProduct AFTER the UPDATE completes
-          deleteProduct();
-        }
-      );
+//     function updateEmployee() {
+//       console.log("Updating all Rocky Road quantities...\n");
+//       var query = connection.query(
+//         "UPDATE products SET ? WHERE ?",
+//         [
+//           {
+//             quantity: 100
+//           },
+//           {
+//             flavor: "Rocky Road"
+//           }
+//         ],
+//         function(err, res) {
+//           if (err) throw err;
+//           console.log(res.affectedRows + " products updated!\n");
+//           // Call deleteProduct AFTER the UPDATE completes
+//           deleteProduct();
+//         }
+//       );
     
-      // logs the actual query being run
-      console.log(query.sql);
-    }
+//       // logs the actual query being run
+//       console.log(query.sql);
+//     }
     
-    function deleteemployee() {
-      console.log("Deleting all strawberry icecream...\n");
-      connection.query(
-        "DELETE FROM products WHERE ?",
-        {
-          flavor: "strawberry"
-        },
-        function(err, res) {
-          if (err) throw err;
-          console.log(res.affectedRows + " products deleted!\n");
-          // Call readProducts AFTER the DELETE completes
-          readProducts();
-        }
-      );
-    }
+//     function deleteemployee() {
+//       console.log("Deleting all strawberry icecream...\n");
+//       connection.query(
+//         "DELETE FROM products WHERE ?",
+//         {
+//           flavor: "strawberry"
+//         },
+//         function(err, res) {
+//           if (err) throw err;
+//           console.log(res.affectedRows + " products deleted!\n");
+//           // Call readProducts AFTER the DELETE completes
+//           readProducts();
+//         }
+//       );
+//     }
     
-    function readProducts() {
-      console.log("Selecting all products...\n");
-      connection.query("SELECT * FROM products", function(err, res) {
-        if (err) throw err;
-        // Log all results of the SELECT statement
-        console.log(res);
-        connection.end();
-      });
-    }
+//     function readProducts() {
+//       console.log("Selecting all products...\n");
+//       connection.query("SELECT * FROM products", function(err, res) {
+//         if (err) throw err;
+//         // Log all results of the SELECT statement
+//         console.log(res);
+//         connection.end();
+//       });
+//     }
     
 
