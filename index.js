c71af929ceda3489bbdd754a1b48f8360e1e4111
Hector Fernandez <hkfernandezdev@gmail.com>
2021-02-04 22:18:25 -0600

    index.js - addEmployee() working and adding records to database

--- index.js
+++ index.js
@@ -4,6 +4,14 @@
 // 3rd party
 const inquirer = require ('inquirer');
 const cTable = require('console.table');
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "rootroot",
+      database: "company_info"
+    });
 // console.table([
 //   {
 //     name: 'foo',
@@ -21,8 +29,12 @@ const Role = require ('./lib/Role.js');
 const questions = require ('./lib/Questions.js');
 
 
-// GLOBAL VARIABLES
-
+//on database connection
+connection.connect(function(err) {
+      if (err) throw err;
+      console.log("connected as id " + connection.threadId + "\n");
+      beginPrompts();
+    });
 
 // FUNCTIONS
 
@@ -48,65 +60,152 @@ function addEmployee () {
       inquirer.prompt (questions.addEmployee)
             .then 
             (({firstName, lastName, employeeRole, managerName})=>{
-                  let employee = new Employee (firstName, lastName, employeeRole, managerName);
-                  pushNewEmployee(employee);
-
+                  let newEmployee = new Employee (firstName, lastName, employeeRole, managerName);
+                  insertNewRecord(newEmployee, 'employees');
             })
 }
-
-function pushNewEmployee (){}
-
-function addDept () {
-      inquirer.prompt (questions.addDept)
-            .then 
-            (({name})=>{
-                  let dept = new Dept (name);
-                  pushNewDept(dept);
-
-            })
-}
-
-function pushNewDept (){}
 
 function addRole () {
       inquirer.prompt (questions.addRole)
             .then 
-            (({roleName, roleDept, roleSalary})=>{
-                  let role = new Role (roleName, roleDept, roleSalary);
-                  pushNewRole(role);
+            (({role_name, role_dept, role_salary})=>{
+                  let newRole = new Role (role_name, role_dept, role_salary);
+                  insertNewRecord(newRole, 'roles');
             })
 }
 
-function pushNewRole (){}
+function insertNewRecord (newObject, tableName){
+      console.log(`Inserting a new record in ${tableName}...\n`);
+      var query = connection.query(
+        `INSERT INTO ${tableName} SET ?`,
+        newObject,
+        function(err, res) {
+          if (err) throw err;
+          console.log(res.affectedRows + ` record inserted!\n`);
+          beginPrompts();
+        }
+      );
+    
+      // logs the actual query being run
+      console.log(query.sql);
+    }
+
+// function addDept () {
+//       inquirer.prompt (questions.addDept)
+//             .then 
+//             (({name})=>{
+//                   let dept = new Dept (name);
+//                   pushNewDept(dept);
+
+//             })
+// }
+
+// function pushNewDept (){}
 
 
-function selectList () {
-      inquirer.prompt (questions.selectList)
-            .then 
-            (({listTypeChoice})=>{
-                  if (listTypeChoice === "Departments") {
-
-                  } else if (listTypeChoice === "Roles") {
-
-                  } else if (listTypeChoice === "employees") {
-
-                  }
-            })
-}
-
-function changeManager () {
-      inquirer.prompt (questions.changeManager)
-            .then 
-            (({employee, manager})=>{
-
-            })
-}
+// function pushNewRole (){}
 
 
+// function selectList () {
+//       inquirer.prompt (questions.selectList)
+//             .then 
+//             (({listTypeChoice})=>{
+//                   if (listTypeChoice === "Departments") {
 
-function init () {
-      beginPrompts()
-}
+//                   } else if (listTypeChoice === "Roles") {
 
- init ();
+//                   } else if (listTypeChoice === "employees") {
+
+//                   }
+//             })
+// }
+
+// function changeManager () {
+//       inquirer.prompt (questions.changeManager)
+//             .then 
+//             (({employee, manager})=>{
+
+//             })
+// }
+
+
+
+// function init () {
+//       beginPrompts()
+// }
+
+//  init ();
+
+//=======================CRUD FUNCTIONS=========================================
+function addDept() {
+      console.log("Inserting a new product...\n");
+      var query = connection.query(
+        "INSERT INTO products SET ?",
+        {
+          flavor: "Rocky Road",
+          price: 3.0,
+          quantity: 50
+        },
+        function(err, res) {
+          if (err) throw err;
+          console.log(res.affectedRows + " product inserted!\n");
+          // Call updateProduct AFTER the INSERT completes
+          updateProduct();
+        }
+      );
+    
+      // logs the actual query being run
+      console.log(query.sql);
+    }
+    
+    function updateEmployee() {
+      console.log("Updating all Rocky Road quantities...\n");
+      var query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+          {
+            quantity: 100
+          },
+          {
+            flavor: "Rocky Road"
+          }
+        ],
+        function(err, res) {
+          if (err) throw err;
+          console.log(res.affectedRows + " products updated!\n");
+          // Call deleteProduct AFTER the UPDATE completes
+          deleteProduct();
+        }
+      );
+    
+      // logs the actual query being run
+      console.log(query.sql);
+    }
+    
+    function deleteemployee() {
+      console.log("Deleting all strawberry icecream...\n");
+      connection.query(
+        "DELETE FROM products WHERE ?",
+        {
+          flavor: "strawberry"
+        },
+        function(err, res) {
+          if (err) throw err;
+          console.log(res.affectedRows + " products deleted!\n");
+          // Call readProducts AFTER the DELETE completes
+          readProducts();
+        }
+      );
+    }
+    
+    function readProducts() {
+      console.log("Selecting all products...\n");
+      connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+        connection.end();
+      });
+    }
+    
 
